c57c6187f7eb69c2bffa2e8314d6fac728a4cea6
Tewodros Wondimu <tewodros.wr@gmail.com>
2017-10-28 21:36:55 +0200

    Added jelly fish into the scene however the texture is not working

--- WhackAJellyFish/ViewController.swift
+++ WhackAJellyFish/ViewController.swift
@@ -40,11 +40,33 @@ class ViewController: UIViewController {
 
     // when the play button is press show a box
     func addNode() {
-        let node = SCNNode(geometry: SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0))
-        node.position = SCNVector3(0,0,-1)
-        node.geometry?.firstMaterial?.diffuse.contents = UIColor.green
-        self.sceneView.scene.rootNode.addChildNode(node)
+        // turbosquid.com sells 3D models
+        // download a 3D model with a dae format
+        // drag it into your project along with the texture file
+        // open the dae file and remove the camera and lights, you should remain with a sphere
+        // In the material inspector, change the diffuse from the color to the texture image
+        // Go to editor, click on convert to sceneKit scene file format
+        // to load the scene in the arkit view you need to have it in an asset
+        // create an asset folder by going to new > file > asset catalog
+        // change the file format to *.scnassets and click use
+        let jellyFishScene = SCNScene(named: "art.scnassets/Jellyfish.scn")
+        
+        // to convert the scene to the node
+        // to get the name go to the scene in the assets and change the name of the child from sphere to jellyfish
+        // recursively will search for every node with in the root node subtree, if false it will only search immediate nodes
+        let jellyFishNode = jellyFishScene?.rootNode.childNode(withName: "Jellyfish", recursively: false)
+        //jellyFishNode?.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "Textures/Jellyfish.png")
+        
+        // place the object infront of you and then add it to the sceneView
+        jellyFishNode?.position = SCNVector3(0,0,-1)
+        self.sceneView.scene.rootNode.addChildNode(jellyFishNode!)
+        
+        // To change the orientation of the 3d model go to assets
+        // then click presepective and make it front
+        // then under the node inspector, change the euler angles to the angle you want (usually x-axis)
+        // to change the size of the 3D object, underscale multiple by a value like 0.1
     }
+    
     
     // Get the tap information
     @objc func handleTap(sender: UITapGestureRecognizer) {
@@ -68,6 +90,7 @@ class ViewController: UIViewController {
             // hit test is an array with one element
             let result = hitTest.first!
             let geometry = result.node.geometry
+            print(geometry ?? "The object that was tapped")
         }
         
     }
